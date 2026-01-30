caeb5d05595212dc8d3d43d6b16378f1ac4e82ee
ayushgoyal0123 <ayushgoyal0123@gmail.com>
2026-01-30 17:29:32 +0530

    1. removed footer from sign up and login page

--- tutor_hub/src/app/app.component.ts
+++ tutor_hub/src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tutor_hub';
+  
+  constructor(private router: Router) {}
+  
+  shouldShowFooter(): boolean {
+    const currentUrl = this.router.url;
+    return !currentUrl.includes('/login') && !currentUrl.includes('/signup');
+  }
 }
